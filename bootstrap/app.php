134cfcfb6c3734b4977d0df6ac3bdcb3497f7227
Abhijit Pal <abhijitpal121@gmail.com>
2025-11-05 08:30:58 +0530

    Api route configrured, Other portals route file configured

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,23 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        // Register custom route files here
+        then: function () {
+            Route::middleware('web')
+                ->prefix('admin')
+                ->group(base_path('routes/admin.php'));
+            Route::middleware('web')
+                ->prefix('teacher')
+                ->group(base_path('routes/teacher.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
