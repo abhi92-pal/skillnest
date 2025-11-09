d91316233aeaf209fb5fe8db7667c3c20fdd8bd8
Abhijit Pal <abhijitpal121@gmail.com>
2025-11-09 07:50:46 +0530

    Route handled

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/{any}', function () {
     return view('app');
-})->where('any', '.*');
+    // })->where('any', '.*');
+})->where('any', '^(?!ad|tc).*$');
+
 
 // Auth::routes();
 
