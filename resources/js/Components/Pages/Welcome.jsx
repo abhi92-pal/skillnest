7e7eb1d230b10894fdbea7d4ef4ec60b16c68e9e
Abhijit Pal <abhijitpal121@gmail.com>
2025-11-15 01:52:31 +0530

    Dynamic Form creation done, Registration Form Component done

--- resources/js/Components/Pages/Welcome.jsx
+++ resources/js/Components/Pages/Welcome.jsx
@@ -6,6 +6,7 @@ import AboutUs from '../AboutUs/AboutUs';
 import Services from '../AboutUs/Services';
 import Testimonial from '../Testimonial/index';
 import Enroll from '../Enroll/index';
+import RegistrationForm from '../Forms/RegistrationForm/RegistrationForm';
 
 const Welcome = () => {
     return (
@@ -19,33 +20,7 @@ const Welcome = () => {
                             <div className="login-wrap p-4 p-md-5">
                                 <h3 className="mb-4">Register Now</h3>
 
-                                <form action="#" className="signup-form">
-                                    <div className="form-group">
-                                        <label className="label" htmlFor="name">Full Name</label>
-                                        <input type="text" className="form-control" placeholder="John Doe" />
-                                    </div>
-
-                                    <div className="form-group">
-                                        <label className="label" htmlFor="email">Email Address</label>
-                                        <input type="text" className="form-control" placeholder="johndoe@gmail.com" />
-                                    </div>
-
-                                    <div className="form-group">
-                                        <label className="label" htmlFor="password">Password</label>
-                                        <input id="password-field" type="password" className="form-control" placeholder="Password" />
-                                    </div>
-
-                                    <div className="form-group">
-                                        <label className="label" htmlFor="confirm-password">Confirm Password</label>
-                                        <input id="confirm-password" type="password" className="form-control" placeholder="Confirm Password" />
-                                    </div>
-
-                                    <div className="form-group d-flex justify-content-end mt-4">
-                                        <button type="submit" className="btn btn-primary submit">
-                                            <span className="fa fa-paper-plane"></span>
-                                        </button>
-                                    </div>
-                                </form>
+                                <RegistrationForm />
 
                                 <p className="text-center">
                                     Already have an account? <a href="#signin">Sign In</a>
