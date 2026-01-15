ed8a0cdc04f7b3e043cd507a148de00b3c534bf5
Abhijit Pal <abhijitpal121@gmail.com>
2026-01-15 08:44:06 +0530

    Player implement -not completed

--- resources/js/student-app/Components/Semester/SemesterAccordion.jsx
+++ resources/js/student-app/Components/Semester/SemesterAccordion.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from '../Utilies/Accordion/Accordion';
+import VideoPlayer from '../Utilies/VideoPlayer/VideoPlayer';
 
 const SemesterAccordion = ({ semesters = [] }) => {
+    const [selectedLesson, setSelectedLesson] = useState(null);
     const containerStyle = {
                     background: 'linear-gradient(135deg, #ce4be8 0%, #207ce5 100%)',
                     padding: '20px',
@@ -17,61 +19,88 @@ const SemesterAccordion = ({ semesters = [] }) => {
                     // textAlign: 'center',
                     marginBottom: '5px'
                 };
+
+    const lessionPlayerHandler = (lesson) => {
+        setSelectedLesson(lesson);
+        setTimeout(() => {
+            window.scrollTo({
+                top: 150,
+                behavior: 'smooth'
+                });
+        }, 0);
+    }
     return (
-        <Accordion
-            accordionHeadingStyle={containerStyle}
-            id="semesterAccordion"
-            items={semesters}
-            renderHeader={(semester) => (
-                <span className="text-white fw-bold">{semester.name}</span>
-            )}
-            renderBody={(semester) => (
-                semester.sem_topics.length == 0 
-                    ? (
-                        <p>Content has not been uploaded yet.</p>
-                    ) : (
-                        // <Accordion
-                        //     topics={semester.sem_topics || []}
-                        //     parentId={semester.id}
-                        // />
+        <>
+            <div>
 
-                        <Accordion
-                            accordionHeadingStyle={container2Style}
-                            id={`lessonAccordion-${semester.id}`}
-                            items={semester.sem_topics || []}
-                            renderHeader={(topic) => (
-                                <strong className='text-white'>{topic.name}</strong>
-                            )}
-                            renderBody={(topic) => (
-                                <>
-                                <p className="text-muted">{topic.description}</p>
-
-                                {topic.lessions?.length ? (
-                                    <div className="row">
-                                    {topic.lessions.map((lesson) => (
-                                        <div key={lesson.id} className="col-md-3 card m-2 p-2">
-                                        <strong>{lesson.name}</strong>
-                                        <p className="mb-0">
-                                            Content Type:{" "}
-                                            {lesson.type === "Text"
-                                            ? "PDF"
-                                            : lesson.type === "Video"
-                                            ? "Video"
-                                            : lesson.type}
-                                        </p>
-                                        </div>
-                                    ))}
-                                    </div>
-                                ) : (
-                                    <p>Content has not been uploaded yet.</p>
+                {selectedLesson && (
+                    <div className="mt-4">
+                        <h5>Now Playing: {selectedLesson.name}</h5>
+                        <VideoPlayer
+                        src={selectedLesson.content_url}
+                        lessonId={selectedLesson.id}
+                        />
+                    </div>
+                    )
+                }
+            </div>
+            <Accordion
+                accordionHeadingStyle={containerStyle}
+                id="semesterAccordion"
+                items={semesters}
+                renderHeader={(semester) => (
+                    <span className="text-white fw-bold">{semester.name}</span>
+                )}
+                renderBody={(semester) => (
+                    semester.sem_topics.length == 0 
+                        ? (
+                            <p>Content has not been uploaded yet.</p>
+                        ) : (
+                            <Accordion
+                                accordionHeadingStyle={container2Style}
+                                id={`lessonAccordion-${semester.id}`}
+                                items={semester.sem_topics || []}
+                                renderHeader={(topic) => (
+                                    <strong className='text-white'>{topic.name}</strong>
                                 )}
-                                </>
-                            )}
+                                renderBody={(topic) => (
+                                    <>
+                                    <p className="text-muted">{topic.description}</p>
+
+                                    {topic.lessions?.length ? (
+                                        <div className="row">
+                                        {topic.lessions.map((lesson) => (
+                                            <div key={lesson.id} 
+                                                className="col-md-3 card m-2 p-2" 
+                                                style={{cursor: 'pointer'}}
+                                                onClick={() => {
+                                                    if (lesson.type === 'Video') {
+                                                        lessionPlayerHandler(lesson);
+                                                    }
+                                                }}>
+                                                <strong>{lesson.name}</strong>
+                                                <p className="mb-0">
+                                                    Content Type:{" "}
+                                                    {lesson.type === "Text"
+                                                    ? "PDF"
+                                                    : lesson.type === "Video"
+                                                    ? "Video"
+                                                    : lesson.type}
+                                                </p>
+                                            </div>
+                                        ))}
+                                        </div>
+                                    ) : (
+                                        <p>Content has not been uploaded yet.</p>
+                                    )}
+                                    </>
+                                )}
                             />
 
-                    )
-            )}
-        />
+                        )
+                )}
+            />
+        </>
     );
 };
 
