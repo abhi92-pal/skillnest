586c19312976f263592c1b12d3a55ba39f90d40e
Sanjib <greet.sanjib@gmail.com>
2026-02-20 10:29:08 +0530

    Teacher Dashboard complete

--- app/Http/Controllers/Teacher/DashboardController.php
+++ app/Http/Controllers/Teacher/DashboardController.php
@@ -3,14 +3,24 @@
 namespace App\Http\Controllers\Teacher;
 
 use App\Http\Controllers\Controller;
+use App\Models\Exampaper;
 use Illuminate\Http\Request;
 
 use App\Models\Lession;
 use App\Models\Topic;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('teacher.dashboard');
+        $auth_user = Auth::user();
+        $pending_exampaper_count = Exampaper::whereHas('topic', function ($query) use ($auth_user) {
+                                                $query->where('author_id', $auth_user->id);
+                                            })->doesntHave('questions')->count();
+
+        $pending_upload_lession_count = Topic::where('author_id', $auth_user->id)
+                            ->doesntHave('lessions')->count();
+
+        return view('teacher.dashboard', compact('pending_exampaper_count','pending_upload_lession_count'));
     }
 }
